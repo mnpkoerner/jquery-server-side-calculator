29ed2927ff5b58c01fad222ab968f1e6a71d590f
mnpkoerner <mnpkoerner@gmail.com>
2020-12-19 10:11:25 -0600

    updated server with switch statment

--- server/server.js
+++ server/server.js
@@ -22,44 +22,68 @@ app.post('/calculate', (req, res) => {
     allCalculations.push(finishedMath)
     res.sendStatus(201)
 })
-
 function doTheMath(obj) {
     let finishedMath = {
-        intOne: 0,
-        intTwo: 0,
-        operator: 0,
+        intOne: obj.intOne,
+        intTwo: obj.intTwo,
+        operator: obj.operator,
         result: 0
     }
-    if (obj.operator === "+") {
-        finishedMath.intOne = obj.intOne
-        finishedMath.intTwo = obj.intTwo
-        finishedMath.operator = obj.operator
-        finishedMath.result = Number(obj.intOne) + Number(obj.intTwo)
-        console.log('plus')
-    }
-    else if (obj.operator === "-") {
-        finishedMath.intOne = obj.intOne
-        finishedMath.intTwo = obj.intTwo
-        finishedMath.operator = obj.operator
-        finishedMath.result = obj.intOne - obj.intTwo
-        console.log('minus')
-    }
-    else if (obj.operator === "*") {
-        finishedMath.intOne = obj.intOne
-        finishedMath.intTwo = obj.intTwo
-        finishedMath.operator = obj.operator
-        finishedMath.result = obj.intOne * obj.intTwo
-        console.log('times')
-    }
-    else if (obj.operator === "/") {
-        finishedMath.intOne = obj.intOne
-        finishedMath.intTwo = obj.intTwo
-        finishedMath.operator = obj.operator
-        finishedMath.result = obj.intOne / obj.intTwo
-        console.log('divide')
+    switch (obj.operator) {
+        case '+':
+            finishedMath.result = Number(obj.intOne) + Number(obj.intTwo);
+            break;
+        case '-':
+            finishedMath.result = obj.intOne - obj.intTwo;
+            break;
+        case '*':
+            finishedMath.result = obj.intOne * obj.intTwo;
+            break;
+        case '/':
+            finishedMath.result = obj.intOne / obj.intTwo;
+            break;
+        default:
+            console.log('server side error in doTheMath, unexpected input')
     }
     return finishedMath
 }
+// function doTheMath(obj) {
+//     let finishedMath = {
+//         intOne: 0,
+//         intTwo: 0,
+//         operator: 0,
+//         result: 0
+//     }
+//     if (obj.operator === "+") {
+//         finishedMath.intOne = obj.intOne
+//         finishedMath.intTwo = obj.intTwo
+//         finishedMath.operator = obj.operator
+//         finishedMath.result = Number(obj.intOne) + Number(obj.intTwo)
+//         console.log('plus')
+//     }
+//     else if (obj.operator === "-") {
+//         finishedMath.intOne = obj.intOne
+//         finishedMath.intTwo = obj.intTwo
+//         finishedMath.operator = obj.operator
+//         finishedMath.result = obj.intOne - obj.intTwo
+//         console.log('minus')
+//     }
+//     else if (obj.operator === "*") {
+//         finishedMath.intOne = obj.intOne
+//         finishedMath.intTwo = obj.intTwo
+//         finishedMath.operator = obj.operator
+//         finishedMath.result = obj.intOne * obj.intTwo
+//         console.log('times')
+//     }
+//     else if (obj.operator === "/") {
+//         finishedMath.intOne = obj.intOne
+//         finishedMath.intTwo = obj.intTwo
+//         finishedMath.operator = obj.operator
+//         finishedMath.result = obj.intOne / obj.intTwo
+//         console.log('divide')
+//     }
+//     return finishedMath
+// }
 
 
 
