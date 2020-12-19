fb990a95511f992b15325e0d1a92c38b59173f95
mnpkoerner <mnpkoerner@gmail.com>
2020-12-18 20:04:26 -0600

    base function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,74 @@
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
     console.log('jq ready')
+    $('#submit').on('click', sendAndAppend)
+    $('#clear').on('click', resetCalculator)
+    $('.operator').on('click', disableOperator)
 }
 
-console.log('js ready')
+let mathObject = {
+    intOne: undefined,
+    intTwo: undefined,
+    operator: null,
+}
+
+function sendAndAppend() {
+    if (mathObject.operator === null) {
+        resetCalculator();
+        alert('Please select a valid operator')
+    }
+    mathObject.intOne = Number($('#inputOne').val());
+    mathObject.intTwo = Number($('#inputTwo').val());
+    //I want to post an object to the server that has two numbers and their operator
+    console.log('in send', mathObject)
+    $.ajax({
+        url: '/calculate',
+        type: 'POST',
+        data: mathObject
+    }).then(function (response) {
+        console.log(response);
+        resetCalculator();
+        //append has the get request, it will run after the user side math
+        //has been posted to the server. when the user data is posted, it
+        //does the math, then stores it in an array in /calculation
+        //the get request accesses calculation
+        append()
+    })
+}
+
+//getarray from /calculate and append dom
+function append() {
+    $.ajax({
+        url: '/calculate',
+        type: 'GET'
+    }).then(function (response) {
+        console.log(response)
+        updateDom(response)
+    })
+}
+
+//clicking any of the operators adds a value to the object
+function disableOperator() {
+    //add operator data-func to the data object we're pushing to the server
+    mathObject.operator = $(this).data('func');
+    //disable all other operator buttons
+    $('.operator').prop('disabled', true);
+    console.log('operators disabled')
+    console.log(mathObject)
+}
+
+//hitting the clear button will clear the number inputs and enable the operator buttons
+function resetCalculator() {
+    $('input').val('');
+    $('.operator').prop('disabled', false);
+}
+
+//update the dom object, the object will be an array of objects I'm storing on the server,
+//the DOM will update every time you click the "=" button
+function updateDom(obj) {
+    $('#target').empty();
+    for (equ of obj) {
+        $('#target').append(`<p>${equ.intOne} ${equ.operator} ${equ.intTwo} = ${equ.result}`)
+    }
+}
--- server/server.js
+++ server/server.js
@@ -4,9 +4,62 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let allCalculations = []
+
+app.get('/calculate', (req, res) => {
+    console.log('you got to /calculate');
+    res.send(allCalculations);
+})
 
 
+
+app.post('/calculate', (req, res) => {
+    console.log('post to /calculate')
+    let mathFromClient = req.body;
+    let finishedMath = doTheMath(mathFromClient);
+    allCalculations.push(finishedMath)
+    res.sendStatus(201)
+})
+
+function doTheMath(obj) {
+    let finishedMath = {
+        intOne: 0,
+        intTwo: 0,
+        operator: 0,
+        result: 0
+    }
+    if (obj.operator === "+") {
+        finishedMath.intOne = obj.intOne
+        finishedMath.intTwo = obj.intTwo
+        finishedMath.operator = obj.operator
+        finishedMath.result = Number(obj.intOne) + Number(obj.intTwo)
+        console.log('plus')
+    }
+    else if (obj.operator === "-") {
+        finishedMath.intOne = obj.intOne
+        finishedMath.intTwo = obj.intTwo
+        finishedMath.operator = obj.operator
+        finishedMath.result = obj.intOne - obj.intTwo
+        console.log('minus')
+    }
+    else if (obj.operator === "*") {
+        finishedMath.intOne = obj.intOne
+        finishedMath.intTwo = obj.intTwo
+        finishedMath.operator = obj.operator
+        finishedMath.result = obj.intOne * obj.intTwo
+        console.log('times')
+    }
+    else if (obj.operator === "/") {
+        finishedMath.intOne = obj.intOne
+        finishedMath.intTwo = obj.intTwo
+        finishedMath.operator = obj.operator
+        finishedMath.result = obj.intOne / obj.intTwo
+        console.log('divide')
+    }
+    return finishedMath
+}
 
 
 
